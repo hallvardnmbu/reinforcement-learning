d321a5b59e16364e6e6205c0d6d8aeadd0e86a5b
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-03-08 21:26:53 +0100

    Cleaned up.

--- reinforcement-learning/frozen-lake/Q.py
+++ reinforcement-learning/frozen-lake/Q.py
@@ -5,38 +5,46 @@ import pandas as pd
 
 
 class TabularQAgent:
-    def __init__(self, state_space, action_space, lr,
-                 gamma, epsilon, epsilon_decay, epsilon_minimum):
+    """Tabular Q-learning agent."""
+    def __init__(self, space, lr, gamma, exploration):
         """
         Classic-style tabular Q learning agent for small, discrete-state games.
 
         Parameters
         ----------
-        state_space : int
-            The number of possible states in the environment.
-        action_space : int
-            The number of possible actions in the environment.
+        space : dict
+            The number of possible states and actions in the environment.
+            Must contain the following keys:
+
+            states : int
+                The number of possible states in the environment.
+            actions : int
+                The number of possible actions in the environment.
         lr : float
             The learning rate for updating the Q-table.
         gamma : float
             The discount factor for future rewards.
-        epsilon : float
+        exploration : dict
             The exploration rate for selecting random actions.
-        epsilon_decay : float
-            The rate at which the exploration rate decays over time.
-        epsilon_minimum : float
-            The minimum exploration rate.
+            Must contain the following keys:
+
+            rate : float
+                The exploration rate for selecting random actions.
+            decay : float
+                The rate at which the exploration rate decays over time.
+            min : float
+                The minimum exploration rate.
         """
-        self.state_space = state_space
-        self.action_space = action_space
+        self.actions = space['actions']
+
         self.lr = lr
         self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_decay = epsilon_decay
-        self.epsilon_minimum = epsilon_minimum
-        self.Q_table = pd.DataFrame(data=np.zeros(shape=(action_space, state_space)),
-                                    columns=[f"State {i}" for i in range(state_space)],
-                                    index=[f"Action {i}" for i in range(action_space)])
+
+        self.exploration = exploration
+
+        self.table = pd.DataFrame(data=np.zeros(shape=(space["actions"], space["states"])),
+                                  columns=[f"State {i}" for i in range(space["states"])],
+                                  index=[f"Action {i}" for i in range(space["actions"])])
 
     def action(self, state):
         """
@@ -53,36 +61,15 @@ class TabularQAgent:
         int
             The selected action to take.
         """
-        if np.random.random() > self.epsilon:
-            action = self.greedy_action(state)
+        if np.random.random() > self.exploration['rate']:
+            action = int(self.table.iloc[:, state].idxmax().split(" ")[-1])
         else:
-            action = np.random.choice(range(self.action_space))
-        self.decrement_epsilon()
+            action = np.random.choice(range(self.actions))
+
+        self.exploration['rate'] = max(self.exploration['min'],
+                                       self.exploration['rate'] - self.exploration['decay'])
+
         return action
-
-    def decrement_epsilon(self):
-        """
-        Decrements the value of epsilon by the epsilon_decay rate, but ensures it doesn't go
-        below epsilon_minimum.
-        """
-        self.epsilon = max(self.epsilon_minimum, self.epsilon - self.epsilon_decay)
-
-    def greedy_action(self, state):
-        """
-        Selects the action with the highest Q-value for the given state.
-
-        Parameters
-        ----------
-        state : int
-            The current state.
-
-        Returns
-        -------
-        int
-            The selected action.
-        """
-        action = self.Q_table.iloc[:, state].idxmax()
-        return int(action.split(" ")[-1])
 
     def learn(self, old_state, action, reward, new_state):
         """
@@ -99,7 +86,9 @@ class TabularQAgent:
         new_state : int
             The state the agent is in after taking the action.
         """
-        Q_observed = reward + self.gamma * self.Q_table.iloc[:, new_state].max()
-        Q_expected = self.Q_table.at[f"Action {action}", f"State {old_state}"]
-        new_Q_val = Q_expected + self.lr * (Q_observed - Q_expected)
-        self.Q_table.at[f"Action {action}", f"State {old_state}"] = new_Q_val
+        observed = reward + self.gamma * self.table.iloc[:, new_state].max()
+        expected = self.table.at[f"Action {action}", f"State {old_state}"]
+
+        updated = expected + self.lr * (observed - expected)
+
+        self.table.at[f"Action {action}", f"State {old_state}"] = updated
